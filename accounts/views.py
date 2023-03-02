05d2146d21bb074a6ed4602b806413d621862812
Kaining <kainingchen2013@gmail.com>
2023-03-02 13:44:39 -0500

    Feature 9 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from accounts.forms import LoginForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -22,3 +22,7 @@ def user_login(request):
         "form": form,
     }
     return render(request, "accounts/login.html", context)
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
